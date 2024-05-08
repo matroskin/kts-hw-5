b2a62deba58ffcc92653ee14db90e4536fd11749
dima kiselev <d.s.matroskin@gmail.com>
2024-05-08 22:48:40 +0300

    add webpack

--- compiled.js
+++ compiled.js
@@ -1,5 +1,6 @@
 "use strict";
 
-const hi = () => {
-  console.log('Hello, World!');
-};
+var _react = _interopRequireDefault(require("react"));
+var _client = _interopRequireDefault(require("react-dom/client"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+_client.default.createRoot(document.getElementById('root')).render( /*#__PURE__*/_react.default.createElement("h1", null, "Hello, world"));
--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-const hi = () => {
-  console.log('Hello, World!');
-};
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <h1>Hello, world</h1>
+);
