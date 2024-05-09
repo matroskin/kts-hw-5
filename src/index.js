cd22f02b010d6baf13cce85f45297a98f1fd2e91
dima kiselev <d.s.matroskin@gmail.com>
2024-05-09 15:16:40 +0300

    add webpack dev server

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <h1>Hello, world</h1>
+  <h1 className='title'>Hello, world</h1>
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,34 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const srcPath = path.resolve(__dirname, 'src');
 const buildPath = path.resolve(__dirname, 'dist');
 
 module.exports = {
-  entry: path.resolve(__dirname, './index.js'),
+  entry: path.join(srcPath, 'index.js'),
   output: {
     path: buildPath,
     filename: "bundle.js"
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+       template: path.join(srcPath, 'index.html')
+     })
+  ],
   module: {
     rules: [
+      {
+        test: /\.css/,
+        use: ['style-loader','css-loader']
+      },
       {
         test: /\.jsx?/,
         use: 'babel-loader'
       }
     ]
+  },
+  devServer: {
+    host: '127.0.0.1',
+    port: 3000
   }
 }
